71b1c6148bfa6d6584d0f6eeff6c9e079019123d
Jordan Lagan <jordanlagan000@gmail.com>
2017-11-10 15:40:46 -0500

    mark to-do items as complete

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -16,6 +16,18 @@ class ItemsController < ApplicationController
   	end
   end
 
+  def destroy
+    @item = Item.find(params[:id])
+    if !@item.destroy
+      flash.now[:alert] = "There was a problem deleting the task."
+    end
+
+    respond_to do |format|
+      format.html
+      format.js
+    end
+  end
+
   private
   def item_params
     params.require(:item).permit(:name)
--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,5 @@
 Rails.application.routes.draw do
 
-  get 'items/create'
-
   devise_for :users
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 
@@ -10,6 +8,6 @@ Rails.application.routes.draw do
   get 'index', to: 'home#index'
 
   resources :users do
-    resources :items, only: [:new, :create]
+    resources :items, only: [:new, :create, :destroy]
   end
 end
