8b4a4687dc7d6f146139b637ca7dfb5c2a8e9a1d
Jordan Lagan <jordanlagan000@gmail.com>
2017-11-17 21:09:29 -0500

    trying to install blocmetrics

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -15,3 +15,12 @@
 //= require_tree .
 //= require jquery
 //= require bootstrap-sprockets
+
+var blocmetrics = {};
+blocmetrics.report = function(eventName) {
+   var event = {event: { name: eventName }};
+   var request = new XMLHttpRequest();
+   request.open("POST", "http://localhost:3000/api/events", true);
+   request.setRequestHeader('Content-Type', 'application/json');
+   request.send(JSON.stringify(event));
+}
